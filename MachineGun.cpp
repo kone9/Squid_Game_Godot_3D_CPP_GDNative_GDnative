97d45cc46936a0f0568babf424f8f881f5663fe4
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-11-17 15:27:29 -0300

    la clase cañon dispara, pero aun tengo que correguir algunas cosas

--- MachineGun.cpp
+++ MachineGun.cpp
@@ -1,9 +1,14 @@
 #include "MachineGun.h"
+#include "BotAI.h"
 
 namespace godot
 {
 	MachineGun::MachineGun()
 	{
+		Timer_shoot = nullptr;
+		bot_to_kill = nullptr;
+		Machine_Gun_Sound = nullptr;
+		is_shooting = false;
 	}
 
 	MachineGun::~MachineGun()
@@ -14,6 +19,9 @@ namespace godot
 	{
 		register_method("_ready", &MachineGun::_ready);
 		register_method("_process", &MachineGun::_process);
+
+		//senials
+		register_method("_on_Timer_shoot_timeout", &MachineGun::_on_Timer_shoot_timeout);
 	}
 
 	void MachineGun::_init()
@@ -22,11 +30,29 @@ namespace godot
 
 	void MachineGun::_ready()
 	{
-		Godot::print("creado machine gun");
+		Timer_shoot = get_node<Timer>("Timer_shoot");
+		Machine_Gun_Sound = get_node<AudioStreamPlayer3D>("Machine_Gun_Sound");
 	}
 
 	void MachineGun::_process(real_t delta)
 	{
+	}
+
+	void MachineGun::shoot(Vector3 look_at_bot)
+	{
+		if (!is_shooting)
+		{
+			is_shooting = true;
+			look_at(look_at_bot, Vector3::UP);//miro al bot
+			Timer_shoot->start();//para activar shoot
+		}
+	}
+
+	//disparo cuando termina el delay
+	void MachineGun::_on_Timer_shoot_timeout()
+	{
+		Machine_Gun_Sound->play();
+		is_shooting = false;
 	}
 
 }
--- MachineGun.h
+++ MachineGun.h
@@ -2,6 +2,10 @@
 
 #include <Godot.hpp>
 #include <Spatial.hpp>
+#include <Timer.hpp>
+#include <AudioStreamPlayer3D.hpp>
+#include <RigidBody.hpp>
+
 
 namespace godot
 {
@@ -17,6 +21,24 @@ namespace godot
 		void _init();
 		void _ready();
 		void _process(real_t delta);
+
+	public:
+		RigidBody* bot_to_kill;
+		AudioStreamPlayer3D* Machine_Gun_Sound;
+		bool is_shooting;//si esta disparando o no
+
+		void shoot(Vector3 look_at_bot);
+
+	private:
+		Timer* Timer_shoot;
+
+	//senials
+	private:
+		void _on_Timer_shoot_timeout();
+
+
+
+
 	};
 }
 
